ad2f3dc93c13f3a02da3a30e6488e7b2fb2de157
AtharvaD11 <67207001+AtharvaD11@users.noreply.github.com>
2021-01-21 12:35:18 +0530

    Create Life_the_Universe_and_Everything.py

---
+++ DSA Learning Series/Contest 1 - Complexity Analysis and Basic Warm Up/Life, the Universe and Everything/Life_the_Universe_and_Everything.py
@@ -0,0 +1,13 @@
+"""
+  AUTHOR -  Atharva Deshpande
+  GITHUB - https://github.com/AtharvaD11
+  QUESTION LINK - https://www.codechef.com/LRNDSA01/problems/TEST
+  """
+
+
+while True:
+    n = int(input())
+    if n== 42:
+        break
+    else:
+        print(n)
