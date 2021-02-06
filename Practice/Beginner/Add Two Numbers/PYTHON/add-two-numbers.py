155321d69fd69ca479fa41bb54317b3a8929da53
AtharvaD11 <67207001+AtharvaD11@users.noreply.github.com>
2021-02-06 17:23:11 +0530

    Create add-two-numbers.py

---
+++ Practice/Beginner/Add Two Numbers/PYTHON/add-two-numbers.py
@@ -0,0 +1,13 @@
+"""
+    AUTHOR - Atharva Deshpande
+    GITHUB PROFILE  -  https://github.com/AtharvaD11
+    QUESTION LINK  -  https://www.codechef.com/problems/FLOW001
+    
+"""
+
+****************************************
+
+t = int(input())
+for _ in range(t):
+    a,b = map(int,input().split())
+    print(a+b)
