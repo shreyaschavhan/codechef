047e9bd97bc4d8d31e9010404e37bb36de3e1296
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2021-03-08 17:41:31 +0530

    Create  Helping Chef.c

---
+++ Practice/Beginner/Helping Chef/C/Helping Chef.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	  int n;
+	  scanf("%d",&n);
+	  if(n>=10){
+	      printf("-1 \n");
+	     }
+	     else{
+	         printf("Thanks for helping Chef! \n ");
+	     }
+	}
+	return 0;
+}
