11e678a05ad339e799bb876606297dff979e9e03
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2021-03-16 10:53:30 +0530

    Create Chef and Remissness.c

---
+++ Practice/Beginner/Chef and Remissness/C/Chef and Remissness.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	 int a,b;
+	 scanf("%d %d",&a,&b);
+	 if(a>b)
+	 {
+	     printf("%d %d\n",a,a+b);
+	}
+	else{
+	    printf("%d %d \n",b,a+b);
+	}
+	}
+	return 0;
+}
+
